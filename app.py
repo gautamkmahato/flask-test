d0b026e546bc7f7acd9768a3028abd72fa9ef1d3
gautamkmahato <gautamkmahato@gmail.com>
2024-10-20 20:21:30 +0530

    second commit

--- app.py
+++ app.py
@@ -1,7 +1,16 @@
 from flask import Flask
+from gevent.pywsgi import WSGIServer
+from dotenv import load_dotenv
+import os
 
 app = Flask(__name__)
+
+PORT = int(os.getenv('PORT', 5050))
 
 @app.route("/")
 def hello_world():
     return "<h1>Hello, World!</h1>"
+
+if __name__ == '__main__':
+    http_server = WSGIServer(('0.0.0.0', PORT), app)
+    http_server.serve_forever()
